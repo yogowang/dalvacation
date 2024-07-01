106a953fb16442e1f7784bba463ea272dbc8c8ce
Zeel Ravalani <zl737848@dal.ca>
2024-07-01 01:52:14 -0300

    Registeration & 1stFactorLogin Lambda Commit

---
+++ Backend/LambdaLogin1stFactor/index.mjs
@@ -0,0 +1,38 @@
+import { CognitoIdentityProviderClient, InitiateAuthCommand } from "@aws-sdk/client-cognito-identity-provider";
+
+const cognitoClient = new CognitoIdentityProviderClient({ region: 'us-east-1' });
+
+export const handler = async (event) => {
+  const { email, password } = JSON.parse(event.body);
+  let responseBody = "";
+  let statusCode = 0;
+
+  try {
+    const authParams = {
+      AuthFlow: 'USER_PASSWORD_AUTH',
+      ClientId: process.env.DalVacationHomeAppClientId,
+      AuthParameters: {
+        USERNAME: email,
+        PASSWORD: password
+      }
+    };
+
+    const authCommand = new InitiateAuthCommand(authParams);
+    const authResponse = await cognitoClient.send(authCommand);
+
+    responseBody = JSON.stringify(authResponse);
+    statusCode = 200;
+  } catch (error) {
+    responseBody = `Login failed: ${error.message}`;
+    statusCode = 500;
+  }
+
+  const response = {
+    statusCode: statusCode,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: responseBody
+  };
+  return response;
+};
