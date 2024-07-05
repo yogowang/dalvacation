af1b768224f01de6f54a659c1f98c5fc7fac4444
Divyank Shah <dv491067@dal.ca>
2024-07-05 07:05:32 -0300

    feat: Home page completed.

--- frontend/src/components/button/SubmitButton.js
+++ frontend/src/components/button/SubmitButton.js
@@ -1,20 +1,13 @@
 import Button from '@mui/material/Button';
-const SubmitButton = ({ buttonName, callButtonFunction }) => {
+const SubmitButton = ({ className, buttonName, callButtonFunction }) => {
   return (
     <Button
       variant="contained"
-      className="w-1/2 bg-blue-500 hover:bg-blue-700 text-white py-2"
+      className={`${className}w-1/2 bg-blue-500 hover:bg-blue-700 text-white py-2`}
       onClick={callButtonFunction}
     >
       {buttonName}
     </Button>
-
-    // <button
-    //   className={`${className} my-2 text-1xl z-5 w-1/2 rounded-lg bg-blue-500 px-8 py-2 text-white shadow-md hover:bg-blue-950 disabled:bg-blue-400`}
-    //   onClick={callButtonFunction}
-    // >
-    //   {buttonName}
-    // </button>
   );
 };
 
