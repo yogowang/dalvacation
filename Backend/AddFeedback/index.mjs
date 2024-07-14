0dda99cb1835b46d0bb2838df0f2aac705cd7085
yuci <yc457137@dal.ca>
2024-07-14 18:53:35 -0300

    refactor complete the add feedback function

--- Backend/AddFeedback/index.mjs
+++ Backend/AddFeedback/index.mjs
@@ -1,11 +1,59 @@
 //https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/dynamodb-example-dynamodb-utilities.html
 //https://codedamn.com/news/javascript/how-to-convert-timestamp-to-date-in-javascript
+//https://console.cloud.google.com/vertex-ai/generative/language/prompt-examples/Sentiment%20analysis
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 import crypto from 'crypto';
+import axios from 'axios';
 const ddbClient = new DynamoDBClient({});
 const dynamo = DynamoDBDocumentClient.from(ddbClient);
+const API_ENDPOINT = process.env.API_ENDPOINT;
+async function analyzeSentiment(text) {
+  const requestData = {
+    contents: [
+      {
+        role: "user",
+        parts: [
+          {
+            text: text
+          },
+        ]
+      }
+    ],
+    generationConfig: {
+      maxOutputTokens: 8192,
+      temperature: 1,
+      topP: 0.95
+    },
+    safetySettings: [
+      {
+        category: "HARM_CATEGORY_HATE_SPEECH",
+        threshold: "BLOCK_MEDIUM_AND_ABOVE"
+      },
+      {
+        category: "HARM_CATEGORY_DANGEROUS_CONTENT",
+        threshold: "BLOCK_MEDIUM_AND_ABOVE"
+      },
+      {
+        category: "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        threshold: "BLOCK_MEDIUM_AND_ABOVE"
+      },
+      {
+        category: "HARM_CATEGORY_HARASSMENT",
+        threshold: "BLOCK_MEDIUM_AND_ABOVE"
+      }
+    ]
+  };
 
+  const response = await axios.post(API_ENDPOINT, requestData, {
+    headers: {
+      "Authorization": `Bearer ${process.env.GOOGLE_API_ACCESS_TOKEN}`,
+      "Content-Type": "application/json"
+    }
+  });
+
+  return response.data;
+}
 export const handler = async (event) => {
   const { email, text } = event;
   const feedback_id=crypto.randomUUID();
@@ -16,6 +64,7 @@ export const handler = async (event) => {
   const tableName = process.env.FeedbackDalVacationDynamoTableName;
 
   try {
+    const sentiment = await analyzeSentiment(text);
       await dynamo.send(
       new PutCommand({
         TableName: tableName,
@@ -24,7 +73,8 @@ export const handler = async (event) => {
           timestamp:timestamp,
           email: email,
           text: text,
-          date:date
+          date:date,
+          sentiment:sentiment
         },
       })
     );
